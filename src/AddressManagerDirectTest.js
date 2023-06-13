770a4f58d331041cba4ad54193c4a4816623f1a7
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-06-13 16:22:25 +0200

    pn-load-test addressManager test

--- src/AddressManagerDirectTest.js
+++ src/AddressManagerDirectTest.js
@@ -1,15 +1,41 @@
 import { check } from 'k6';
+import exec from 'k6/execution';
 import http from 'k6/http';
 
 
 export let options = JSON.parse(open('./modules/test-types/'+__ENV.TEST_TYPE+'.json'));
-
 let addressManagerRequest = JSON.parse(open('./model/requestAddressManager.json'));
 
+let iunFile = open('./resources/NotificationIUN.txt');
+let capArray = ['20121','20131','20141','20151', '20161', '20122', '20132', '20142', '20152','20162', '20123', '20133',
+ '20143', '20153', '20124', '20134', '20144', '20154', '20125', '20135', '20145', '20155', '20126', '20136', '20146',
+  '20156', '20127', '20137', '20147', '20157', '20128', '20138', '20148', '20158', '20129', '20139','20149', '20159'];
 
-export default function preloadFileDirect() {
- 
-    let url = 'http://localhost:8888/address-private/normalize';
+export function setup() {
+    let useIunFile = `${__ENV.USE_IUN_FILE}`
+    if(useIunFile && useIunFile !== 'undefined') {
+      let iunArray = iunFile.split(';');
+      console.log("IUN_LENGTH: "+ iunArray.length);
+      return iunArray
+    }
+    return sendNotificationToPn("FRMTTR76M06B715E").iun;
+}
+
+export default function addressManagerDirect(iun) {
+
+    let useIunFile = `${__ENV.USE_IUN_FILE}`
+    let currentIun = iun;
+    if(useIunFile && useIunFile !== 'undefined') {
+        currentIun = iun[exec.scenario.iterationInTest % iun.length].trim();
+    }
+
+    addressManagerRequest.correlationId = 'VALIDATE_NORMALIZE_ADDRESSES_REQUEST_IUN_'+currentIun;
+    addressManagerRequest.requestItems[0].id = exec.scenario.iterationInTest;
+    addressManagerRequest.requestItems[0].cap= capArray[exec.scenario.iterationInTest % capArray.length].trim();
+    addressManagerRequest.requestItems[0].addressRow = 'VIA Mazzini '+exec.scenario.iterationInTest;
+
+
+    let url = 'http://internal-EcsA-20230406081307135800000004-20484703.eu-south-1.elb.amazonaws.com:8080/address-private/normalize';
     let paramsAddressManager = {
         headers: {
             'Content-Type': 'application/json',
