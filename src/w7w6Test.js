bff117bbc0229044abf84bbb8e19a8053145d25a
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2024-10-01 23:32:47 +0200

    Revert "update"
    This reverts commit a6ae8d4f6072752697dbcf4c2927b7239f5ef96b.

--- src/w7w6Test.js
+++ src/w7w6Test.js
@@ -22,8 +22,8 @@ export const options = {
 
         stages: [
           { target: 5, duration: '10s' },
-          { target: 10, duration: '15m' },
-          { target: 10, duration: '10m' },
+          { target: 40, duration: '15m' },
+          { target: 40, duration: '60m' },
           { target: 5, duration: '0s' },
           { target: 5, duration: '10s' }
         ],
@@ -38,11 +38,11 @@ export const options = {
         maxVUs: 9000,
 
         stages: [
-          { target: 3, duration: '10s' },
-          { target: 5, duration: '15m' },
-          { target: 5, duration: '10m' },
-          { target: 3, duration: '0s' },
-          { target: 3, duration: '10s' }
+          { target: 5, duration: '10s' },
+          { target: 10, duration: '15m' },
+          { target: 10, duration: '60m' },
+          { target: 5, duration: '0s' },
+          { target: 5, duration: '10s' }
         ],
         tags: { test_type: 'digitalSoakTest' }, 
         exec: 'digitalSoakTest', 
