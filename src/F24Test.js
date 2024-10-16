70356601bffe3e3b3b7fc204e7a0a0c346c23e99
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2024-10-16 16:38:20 +0200

    update f24 test

--- src/F24Test.js
+++ src/F24Test.js
@@ -43,7 +43,7 @@ const fileArray = new SharedArray('bin file sharedArray attach', function () {
 const f24Array = new SharedArray('bin file sharedArray F24', function () {
     const dataArray2 = [];
     
-    var obj2 = {'fileString': encoding.b64encode(open('./resources/METADATA_f24.json'))}
+    var obj2 = {'fileString': encoding.b64encode(open('./resources/METADATA_STANDARD_GRANDE.json'))}
     dataArray2.push(obj2);
     
     return dataArray2; // must be an array
