2b6fda4ce08b86c7c530e990cca72cd32f01b77e
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2025-03-13 17:36:28 +0100

    create w7OnlySend

---
+++ src/W7OnlySend.js
@@ -0,0 +1,26 @@
+import { sleep } from 'k6';
+import w7TestOptimized from './W7Test.js';
+
+
+
+
+export let options = JSON.parse(open('./modules/test-types/'+__ENV.TEST_TYPE+'.json'));
+
+
+
+/**
+ * This test is the optimized version of the DeliverSendAndReceiverGetDownload.js test
+ * 
+ * The K6 memory leak is partially caused by the use of external modules
+*/
+export default function w6onlySend() {
+
+
+  try{
+    w7TestOptimized(true);
+  }catch(error){
+    console.log('internalSendNotification error: ',error)
+  }
+  
+  sleep(2);
+}
