c794653cba5989573fe328f2ec3d62d5c7e2227e
Matteo Mancini <matteo.mancini@alten.it>
2025-04-01 10:52:31 +0200

    added tracingId text file

--- src/InteropTracing.js
+++ src/InteropTracing.js
@@ -3,11 +3,11 @@ import exec from 'k6/execution';
 import http from 'k6/http';
 import { Counter } from 'k6/metrics';
 
-const AVERAGE_FILES_UPLOADED_PER_DAY = 50;
+const AVERAGE_FILES_UPLOADED_PER_DAY = 7500;
 
 export let options = JSON.parse(open('./modules/test-types/'+__ENV.TEST_TYPE+'.json'));
 
-let bearerToken = `dummybearer`
+let bearerToken = `eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJSUzI1NiIsInVzZSI6InNpZyIsImtpZCI6IjFmM2U2NTdkLTQ3NGQtNDFjNi1iNDhiLTk3ZGRlZGE4MWJjMiJ9.eyJvcmdhbml6YXRpb25JZCI6ImU3OWEyNGNkLThlZGMtNDQxZS1hZThkLWU4N2MzYWVhMDA1OSIsInVpZCI6ImYwN2RkYjhmLTE3ZjktNDdkNC1iMzFlLTM1ZDFhYzEwZTUyMSIsImF1ZCI6InFhLmludGVyb3AucGFnb3BhLml0L20ybSIsIm5iZiI6MS43NDM0OTIwMTlFOSwib3JnYW5pemF0aW9uIjp7ImlwYUNvZGUiOiI1TjJUUjU1NyIsImlkIjoiYTA2MDVkMWMtMmE4Zi00MDBlLTkxODUtNDY0NDEyMTVhODRhIiwibmFtZSI6IlBhZ29QQSBTLnAuQS4iLCJmaXNjYWxfY29kZSI6IjE1Mzc2MzcxMDA5Iiwicm9sZXMiOlt7InJvbGUiOiJhZG1pbiIsInBhcnR5Um9sZSI6Ik1BTkFHRVIifV19LCJpc3MiOiJxYS5pbnRlcm9wLnBhZ29wYS5pdCIsImV4dGVybmFsSWQiOnsib3JpZ2luIjoiSVBBIiwidmFsdWUiOiJjX2YyMDUifSwidXNlci1yb2xlcyI6ImFkbWluIiwic2VsZmNhcmVJZCI6IjAyNmU4YzcyLTc5NDQtNGRjZC04NjY4LWY1OTY0NDdmZWM2ZCIsImV4cCI6MS43NDM1Nzg0MTlFOSwiaWF0IjoxLjc0MzQ5MjAxOUU5LCJqdGkiOiIzYjcyNzhlZS0yMDk4LTQzYTktYjk5NC0wODBiMWJkNDk5NGUifQ.dKTEAJVQJXSIMnChVpvrUKWVuoJJlhdPSa46y2RAcZeGnf3IDQ00hrmlra8u9n2WgMjHVfxo6cJC5tieKD2TpDo0MDOnce4f-R_9V7DVZZg35MaxLSktT-cAGOs3EdO9eSqgIcmxRH15b-hZh42GeHA1-KNk_PLW1E2KT83NOqivW16o2v-5t5T4XbwX4y76HBFXyfH8cD7yrZ9UnzeCUai3AthzGJF1T6h83vrcnkEqbcxkVRG52JZH2qKPCwkraZu512wb3WsQWaBjHRYfZuI0NCBmTGxuIUTIJIVIoNUrRT0pvyR0Ke2YGJ7TZciTiVX4ikw1y4PxkixOzJ39Bw`
 
 let params = {
   headers: {
@@ -15,8 +15,26 @@ let params = {
   },
 };
 
+options.setupTimeout = options.setupTimeout || "38600s";
+
+
+
+let tracingIdFile = open('./resources/TracingID-QA.txt');
 
 export function setup() {
+    if(tracingIdFile !== 'undefined') {
+      let lines = tracingIdFile.split(';');
+      const tracingIdArray = lines.map(line => {
+        const [tracingId, date] = line.split(',');
+        return { tracingId, date };
+      });
+      console.log("TRACING-ID_LENGTH: "+ tracingIdArray.length);
+      return tracingIdArray
+    }
+    throw new Error('Setup failed: there are no Tracing-ID loaded!');
+}
+
+/*export function setup() {
   let tracingList = [];
   let offset = 0;
   let addedTraces = 0;
@@ -53,9 +71,9 @@ export function setup() {
   if (tracingList.length < AVERAGE_FILES_UPLOADED_PER_DAY) {
     throw new Error('Setup failed: there are no enough Tracing loaded!')
   }
-  console.log(tracingList);
+  console.log(tracingList.map(item => `${item.tracingId},${item.date};`).join('\n'));
   return { tracingList };
-}
+}*/
 
 const code_success = new Counter('code_success');
 const code_unauthorized = new Counter('code_unauthorized');
@@ -68,11 +86,11 @@ const code_serviceunavailable = new Counter('code_serviceunavailable');
 
 
 export default function replaceTracingTest(data) {
-  const successFile = `date,purpose_id,status,token_id,requests_count\n${data.tracingList[exec.scenario.iterationInTest].date},59b568cc-a097-4217-9c58-66fa76389fe0,200,c7ebf39e-e323-46f1-92d8-0482bcc3bfe3,555\n${data.tracingList[exec.scenario.iterationInTest].date},59b568cc-a097-4217-9c58-66fa76389fe0,404,c7ebf39e-e323-46f1-92d8-0482bcc3bfe3,0`;
+  const successFile = `date,purpose_id,status,token_id,requests_count\n${data[exec.scenario.iterationInTest].date},59b568cc-a097-4217-9c58-66fa76389fe0,200,c7ebf39e-e323-46f1-92d8-0482bcc3bfe3,555\n${data[exec.scenario.iterationInTest].date},59b568cc-a097-4217-9c58-66fa76389fe0,404,c7ebf39e-e323-46f1-92d8-0482bcc3bfe3,0`;
   const formData = {
     file: http.file(successFile, 'data.csv', 'text/csv')
   };
-  let url = `https://api.qa.tracing.interop.pagopa.it/tracings/${data.tracingList[exec.scenario.iterationInTest].tracingId}/replace`;
+  let url = `https://api.qa.tracing.interop.pagopa.it/tracings/${data[exec.scenario.iterationInTest].tracingId}/replace`;
   
   let tracingResponse = http.post(url, formData, params );
 
