9acc4d2ed5324b55e65937eaee1586687d0f3974
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-17 16:46:05 +0200

    feature: add EServiceCatalogTest.js

---
+++ src/common/test-types/loadOptions.js
@@ -0,0 +1,8 @@
+// src/common/test-types/loadOptions.js
+export function loadTestOptions(testType = __ENV.TEST_TYPE) {
+  if (!testType) {
+    throw new Error('Missing TEST_TYPE env var');
+  }
+
+  return JSON.parse(open(import.meta.resolve(`./${testType}.json`)));
+}
