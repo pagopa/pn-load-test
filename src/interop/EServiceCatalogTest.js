a6a0d2ed1f19129572bc7d717e15f16aa8dbe097
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-17 16:46:09 +0200

    refactor: rename DeliverySendNotification.js to EServiceCatalogTest.js and update function for EService catalog retrieval

---
+++ src/interop/EServiceCatalogTest.js
@@ -0,0 +1,34 @@
+import {loadTestOptions} from '../common/test-types/loadOptions.js';
+import { check, sleep } from 'k6';
+import http from 'k6/http';
+
+export let options = loadTestOptions();
+
+let bearerToken = `${__ENV.ADMIN_GSP_TOKEN}`
+let bffBasePath = `${__ENV.BFF_BASE_PATH}`
+
+export default function getEServiceCatalog() {
+    let url = `https://${bffBasePath}/catalog?offset=0&limit=50`;
+    let authHeader = 'Bearer ' + bearerToken;
+
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        },
+    };
+
+    let r = http.get(url, params);
+
+    console.log(`EService Catalog BFF Received. Status code: ${r.status}`);
+
+    check(r, {
+        'status code is 200': (r) => r.status === 200,
+    });
+
+    console.log(JSON.parse(r.body))
+
+    sleep(1);
+
+    return r;
+}
