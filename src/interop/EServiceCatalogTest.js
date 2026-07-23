a81f0d824d4aef8a27c058d96831ded8bd8502f6
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-07-23 15:57:10 +0200

    refactor: restore loadTestOptions import and update options initialization in EServiceCatalogTest.js

--- src/interop/EServiceCatalogTest.js
+++ src/interop/EServiceCatalogTest.js
@@ -1,9 +1,8 @@
-//import {loadTestOptions} from '../common/test-types/loadOptions.js';
+import {loadTestOptions} from '../common/test-types/loadOptions.js';
 import { check, sleep } from 'k6';
 import http from 'k6/http';
 
-//export let options = loadTestOptions();
-export let options = JSON.parse(open('./common/test-type/'+__ENV.TEST_TYPE+'.json'));
+export let options = loadTestOptions();
 
 let bearerToken = `${__ENV.ADMIN_GSP_TOKEN}`
 let bffBasePath = `${__ENV.BFF_BASE_PATH}`
